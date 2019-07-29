ec87556751d2aab4fe7acd3b6218af57882c8242
Takeshi Watanabe <take-cheeze@users.noreply.github.com>
2019-07-29 19:26:17 +0900

    Fix download script in nncompress

--- nncompress/scripts/download_glove_data.sh
+++ nncompress/scripts/download_glove_data.sh
@@ -2,7 +2,7 @@
 
 # this implementation is forked from https://github.com/zomux/neuralcompressor
 rm data/glove*
-curl -o data/glove.6B.zip https://nlp.stanford.edu/data/glove.6B.zip
+curl -L -o data/glove.6B.zip https://nlp.stanford.edu/data/glove.6B.zip
 cd data
 unzip glove.6B.zip
 cd ..
